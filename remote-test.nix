98724f64112ca2a21b8a6eeef6a71caed99f1488
justinwoo <moomoowoo@gmail.com>
2018-10-22 01:29:58 +0300

    refactor some nix lang

--- remote-test.nix
+++ remote-test.nix
@@ -1,27 +1,15 @@
 let
   pkgs = import <nixpkgs> {};
 
-  remote-src = pkgs.fetchFromGitHub {
+  remote = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "8d02bf2ce370a4f9a6997094b8eaa0a808372679";
-    sha256 = "0b8da75vg9fgaga4v236ibycqski4138s35l35fciz3ydmzbf8hf";
-  };
+    rev = "de9275d9e6635addb5c8448e4ca354a62570fd49";
+    sha256 = "14g2yrfigsp0z87jk4aand8g1w99fsmfsdbg6zrvkmf4nmjhs074";
+  });
+in pkgs.stdenv.mkDerivation {
+  name = "remote-test";
+  src = ./.;
 
-  remote = import remote-src;
-in {
-  easy = pkgs.stdenv.mkDerivation {
-    name = "easy-purescript-nix";
-    src = ./.;
-
-    buildInputs = [
-      remote.purs
-      remote.psc-package-simple
-      remote.purp
-
-      remote.dhall-simple
-      remote.dhall-json-simple
-      remote.spacchetti-cli
-    ];
-  };
+  buildInputs = remote.buildInputs;
 }
