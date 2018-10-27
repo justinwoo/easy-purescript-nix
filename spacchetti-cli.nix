45050a69fc60b9e7259053a9d68d42a0e47dbf33
justinwoo <moomoowoo@gmail.com>
2018-10-27 12:55:37 +0300

    update spacchetti cli

--- spacchetti-cli.nix
+++ spacchetti-cli.nix
@@ -2,11 +2,11 @@
 
 pkgs.stdenv.mkDerivation rec {
   name = "spacchetti";
-  version = "0.4.0.0";
+  version = "0.5.0.0";
 
   src = pkgs.fetchurl {
     url = "https://github.com/justinwoo/spacchetti-cli/releases/download/${version}/linux.tar.gz";
-    sha256 = "0ngp1x85darqx0hlyl9cajjc8gh6lmp3pkpxmsb338rsk72nwx6l";
+    sha256 = "1rgkwxhvzy5scjn5v0hf988k8sdmm3qa9agj9swxq6w2h80i1lnr";
   };
 
   unpackPhase = ''
