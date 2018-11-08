0b617910bdcabd855e70c66b0ec2ee5dfe4c6f7a
justinwoo <moomoowoo@gmail.com>
2018-11-08 13:06:34 +0200

    inherit pkgs in derivations

--- default.nix
+++ default.nix
@@ -2,15 +2,15 @@ let
   pkgs = import <nixpkgs> {};
 
   inputs = {
-    purs = import ./purs.nix {};
-    psc-package-simple = import ./psc-package-simple.nix {};
-    purp = import ./purp.nix {};
+    purs = import ./purs.nix { inherit pkgs; };
+    psc-package-simple = import ./psc-package-simple.nix { inherit pkgs; };
+    purp = import ./purp.nix { inherit pkgs; };
 
-    dhall-simple = import ./dhall-simple.nix {};
-    dhall-json-simple = import ./dhall-json-simple.nix {};
-    spacchetti-cli = import ./spacchetti-cli.nix {};
+    dhall-simple = import ./dhall-simple.nix { inherit pkgs; };
+    dhall-json-simple = import ./dhall-json-simple.nix { inherit pkgs; };
+    spacchetti-cli = import ./spacchetti-cli.nix { inherit pkgs; };
 
-    psc-package2nix = import ./psc-package2nix.nix {};
+    psc-package2nix = import ./psc-package2nix.nix { inherit pkgs; };
   };
 
   buildInputs = builtins.attrValues inputs;
