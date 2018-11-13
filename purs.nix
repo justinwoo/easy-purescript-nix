c6f4e8c4babe51b30aa44c7f6cac426775ab9afb
justinwoo <moomoowoo@gmail.com>
2018-11-13 10:28:27 +0200

    purs 0.12.1

--- purs.nix
+++ purs.nix
@@ -3,11 +3,11 @@
 let dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
 in pkgs.stdenv.mkDerivation rec {
   name = "purs-simple";
-  version = "v0.12.0";
+  version = "v0.12.1";
 
   src = pkgs.fetchurl {
     url = "https://github.com/purescript/purescript/releases/download/${version}/linux64.tar.gz";
-    sha256 = "1wf7n5y8qsa0s2p0nb5q81ck6ajfyp9ijr72bf6j6bhc6pcpgmyc";
+    sha256 = "01az5127g7jpznsjvpkrl59i922fc5i219qdvsrimzimrv08mr18";
   };
 
   buildInputs = [ pkgs.zlib
