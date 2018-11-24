77e228869cf53e53449f5d883160e5ab59544708
justinwoo <moomoowoo@gmail.com>
2018-11-24 16:00:39 +0200

    fix dhall json link

--- dhall-json-simple.nix
+++ dhall-json-simple.nix
@@ -3,9 +3,10 @@
 pkgs.stdenv.mkDerivation rec {
   name = "dhall-json-simple";
   version = "1.2.5";
+  dhall-version = "1.19.0";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/dhall-lang/dhall-haskell/releases/download/${version}/dhall-json-${version}-x86_64-linux.tar.bz2";
+    url = "https://github.com/dhall-lang/dhall-haskell/releases/download/${dhall-version}/dhall-json-1.2.5-x86_64-linux.tar.bz2";
     sha256 = "0dikk4dykggdv89i55jy38njnv53ms747s5lvhd2r86q2ja7a1r1";
   };
 
