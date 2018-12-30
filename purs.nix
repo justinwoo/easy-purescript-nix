11026b85a5010bb211288c63cd9193f0366fa9ac
Jonny <jonny.covert@gmail.com>
2018-12-29 21:50:20 -0500

    Got MacOS working with purs. Need to test if linux still works with it.

--- purs.nix
+++ purs.nix
@@ -5,10 +5,30 @@ in pkgs.stdenv.mkDerivation rec {
   name = "purs-simple";
   version = "v0.12.1";
 
+  platformStr =
+    if pkgs.stdenv.isDarwin
+      then "macos"
+      else "linux64";
+
+  sha256 =
+    if pkgs.stdenv.isDarwin
+      then "13fqpi2c6k5fw2cnzxghrlqpznvlwdcf3yizmf9zbz2zfmfihmig"
+      else "01az5127g7jpznsjvpkrl59i922fc5i219qdvsrimzimrv08mr18";
+
   src = pkgs.fetchurl {
-    url = "https://github.com/purescript/purescript/releases/download/${version}/linux64.tar.gz";
-    sha256 = "01az5127g7jpznsjvpkrl59i922fc5i219qdvsrimzimrv08mr18";
+    url = "https://github.com/purescript/purescript/releases/download/${version}/${platformStr}.tar.gz";
+    sha256 = sha256;
   };
+
+  patchelf =
+    if pkgs.stdenv.isDarwin
+      then ""
+      else
+        ''
+          chmod u+w $PURS
+          patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
+          chmod u-w $PURS
+        '';
 
   buildInputs = [ pkgs.zlib
                   pkgs.gmp
@@ -21,9 +41,7 @@ in pkgs.stdenv.mkDerivation rec {
     PURS="$out/bin/purs"
 
     install -D -m555 -T purs $PURS
-    chmod u+w $PURS
-    patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURS
-    chmod u-w $PURS
+    ${patchelf}
 
     mkdir -p $out/etc/bash_completion.d/
     $PURS --bash-completion-script $PURS > $out/etc/bash_completion.d/purs-completion.bash
