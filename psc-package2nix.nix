a62866fa1c6493cad5d07123a025347f2a0b6440
justinwoo <moomoowoo@gmail.com>
2019-01-29 17:07:01 +0200

    update psc-package2nix

--- psc-package2nix.nix
+++ psc-package2nix.nix
@@ -3,8 +3,8 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "psc-package2nix";
-  rev = "f0d76ecb1e1f3b0665cc72ce3a4a656496a0e8d1";
-  sha256 = "0mg3pxggag7yl4jkzj6mhp0cjbc7q19k37y7crzl6ki9cbrq2p6a";
+  rev = "816e72e91dae2fff8ef99808bd084c535e1be068";
+  sha256 = "0h51dzcchw177x2pgqxi0md0mcmsx649h2p0j0vgjr57h9vnl5vj";
 }) {
   inherit pkgs;
 }
