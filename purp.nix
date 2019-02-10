0d830810e0ed805c95307e77f93bde1f5f8d4962
justinwoo <moomoowoo@gmail.com>
2019-02-10 22:09:48 +0200

    update purp

--- purp.nix
+++ purp.nix
@@ -1,31 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
-let dynamic-linker = pkgs.stdenv.cc.bintools.dynamicLinker;
-in pkgs.stdenv.mkDerivation rec {
-  name = "purp";
-  version = "0.3.0.0";
-
-  src = pkgs.fetchurl {
-    url = "https://github.com/justinwoo/purp/releases/download/${version}/linux.tar.gz";
-    sha256 = "0sz9mc5rdj59kxq370dffd2k4zamgxj8m3hr8d8kmzzrmpp4qk66";
-  };
-
-  buildInputs = [ pkgs.gmp ];
-  libPath = pkgs.lib.makeLibraryPath buildInputs;
-  dontStrip = true;
-
-  unpackPhase = ''
-    mkdir -p $out/bin
-    tar xf $src -C $out/bin
-
-    PURP=$out/bin/purp
-    chmod u+w $PURP
-    patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PURP
-    chmod u-w $PURP
-
-    mkdir -p $out/etc/bash_completion.d/
-    $PURP --bash-completion-script $PURP > $out/etc/bash_completion.d/purp-completion.bash
-  '';
-
-  dontInstall = true;
-}
+import (pkgs.fetchFromGitHub {
+  owner = "justinwoo";
+  repo = "purp";
+  rev = "e4c758c04b825b9c29eb76d7965df6c48f13def2";
+  sha256 = "155zinqxwmy5sjmia3n9h4g2faj174fi2wlnh1b9mh13nm5rd2hh";
+}) { inherit pkgs; }
