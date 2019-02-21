f471dd5bfb71a09e130544a2c3c42fe4bdfb2fc6
justinwoo <moomoowoo@gmail.com>
2019-02-21 21:03:53 +0200

    updates

--- psc-package2nix.nix
+++ psc-package2nix.nix
@@ -3,8 +3,8 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "psc-package2nix";
-  rev = "b6025852ab1067577c33d9011fbd7eae5adf0ded";
-  sha256 = "03k94k5y89dabi6bd85hm6jnps4bcjalq64r77gn69dyw33j1465";
+  rev = "4135235e9577bb130e23b33931a2e228a34fcd34";
+  sha256 = "087641lyvpwvd51qc3zgfk8nb4n7fhp0cmg4f2f7rvm9k0ha456v";
 }) {
   inherit pkgs;
 }
--- remote-test.nix
+++ remote-test.nix
@@ -3,8 +3,8 @@ let
   remote = import (pkgs.fetchFromGitHub {
     owner = "justinwoo";
     repo = "easy-purescript-nix";
-    rev = "15ae0484841c4d984fdb59d9af3d13902777f5ea";
-    sha256 = "08505jj83qnc9n7c98y6sb0cdyczzgmpi1cyjbg8jl9l648537ml";
+    rev = "91e293a2625cd9d58fc514894dfdff22b4ee82c1";
+    sha256 = "1rd18wy9jcdx65by04qr852wdwar31d0h5j6dykaymifj6qajh8d";
   });
 in pkgs.runCommand "easy-purescript-remote-test" {
   buildInputs = remote.buildInputs;
