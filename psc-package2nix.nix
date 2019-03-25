92d4230a645572d9f4df68d38b77e3d28a90db18
justinwoo <moomoowoo@gmail.com>
2019-03-25 11:59:49 +0200

    update psc-package2nix
    due to changes to nixpkgs haskell packages that mark packages as broken

--- psc-package2nix.nix
+++ psc-package2nix.nix
@@ -3,8 +3,8 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "psc-package2nix";
-  rev = "73ed20532413fe27fd6df78b9f690feab7b65bd7";
-  sha256 = "1hgs0bwzvfv5mlcby6gr4m8algnm2xxii0cdn53ya3fx66m6dz2z";
+  rev = "b4d6a834ac124440a503f0510b8a9de95532b16c";
+  sha256 = "0g9fq4j472bcr1x5na6mzr3av95xhvdmnlns1ncvsl4kqa8ix2zr";
 }) {
   inherit pkgs;
 }
