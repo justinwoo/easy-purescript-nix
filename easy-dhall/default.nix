988759133855e33d82f58981f4f1c9602211f7a1
Serhii Khoma <srghma@gmail.com>
2019-05-30 23:52:07 +0300

    feat: easy-dhall -> add update-revision.sh

--- easy-dhall/default.nix
+++ easy-dhall/default.nix
@@ -1,13 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  revData = builtins.fromJSON (builtins.readFile ./revision.json);
-  owner   = builtins.elemAt (builtins.match "https?://.*/(.*)/.*" revData.url) 0;
-  repo    = builtins.elemAt (builtins.match "https?://.*/.*/(.*)" revData.url) 0;
-
-  src = pkgs.fetchFromGitHub {
-    inherit owner repo;
-    inherit (revData) rev sha256;
-  };
+  src = pkgs.fetchFromGitHub (
+    builtins.fromJSON (
+      builtins.readFile ./revision.json
+    )
+  );
 in
   import src { inherit pkgs; }
