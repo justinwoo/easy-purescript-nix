618a42b4829e3d92ced6fda1341871a5da014c60
Serhii Khoma <srghma@gmail.com>
2019-05-31 22:10:30 +0300

    feat: psc-package2nix -> add update.sh

--- psc-package2nix/default.nix
+++ psc-package2nix/default.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
-import (pkgs.fetchFromGitHub {
-  owner = "justinwoo";
-  repo = "psc-package2nix";
-  rev = "6e8f6dc6dea896c71b30cc88a2d95d6d1e48a6f0";
-  sha256 = "0fa6zaxxmqxva1xmnap9ng7b90zr9a55x1l5xk8igdw2nldqfa46";
-}) {
-  inherit pkgs;
-}
+let
+  src = pkgs.fetchFromGitHub (
+    builtins.fromJSON (
+      builtins.readFile ./revision.json
+    )
+  );
+in
+  import src { inherit pkgs; }
---
+++ psc-package2nix/update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p nix-prefetch-git jq
+
+# prelude
+script_dir=$(dirname "$(readlink -f "$BASH_SOURCE")")
+
+# config
+owner="justinwoo"
+repo="psc-package2nix"
+
+# script
+output=$(nix-prefetch-git https://github.com/$owner/$repo)
+rev=$(echo "$output" | jq --raw-output .rev)
+sha256=$(echo "$output" | jq --raw-output .sha256)
+
+cat > "$script_dir/revision.json" <<EOF
+{
+  "owner": "$owner",
+  "repo": "$repo",
+  "rev": "$rev",
+  "sha256": "$sha256"
+}
+EOF
