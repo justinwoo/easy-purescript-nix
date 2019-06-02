ba3f533d027695eec9178517c61bda90ea315ffd
justinwoo <moomoowoo@gmail.com>
2019-06-02 09:25:03 +0200

    update dhall

--- easy-dhall.nix
+++ easy-dhall.nix
@@ -3,6 +3,6 @@
 import (pkgs.fetchFromGitHub {
   owner = "justinwoo";
   repo = "easy-dhall-nix";
-  rev = "d49cdc77be50dd2506576fc31931297620278bec";
-  sha256 = "1gmvaw6wnl5ps1g30sgzy6gy2500dvg3nmbw56h85jlzhficspn6";
+  rev = "0ebdec46cbeee62b7cb06847ca38cc6fa09e0ff2";
+  sha256 = "1qnm5c2q46lkdm39gjyfp35anhz5sil6wknazg8s2ldh3c6znhxj";
 }) {}
