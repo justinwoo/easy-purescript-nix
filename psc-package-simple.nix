4176669b3d1ecf17b56e1a88295c7c9e9933fcaa
Myron Wu <myronwu@gmail.com>
2019-08-19 16:11:33 -0700

    Getting zephyr and psc-package working on Darwin

--- psc-package-simple.nix
+++ psc-package-simple.nix
@@ -8,10 +8,15 @@ in pkgs.stdenv.mkDerivation rec {
 
   version = "v0.5.1";
 
-  src = pkgs.fetchurl {
-    url = "https://github.com/purescript/psc-package/releases/download/v0.5.1/linux64.tar.gz";
-    sha256 = "0nffiqhfdfkg0i9hgz3vhnjrwsb4gg912rmsn2gdfgbh1m44k5df";
-  };
+  src = if pkgs.stdenv.isDarwin
+    then pkgs.fetchurl {
+      url = "https://github.com/purescript/psc-package/releases/download/v0.5.1/macos.tar.gz";
+      sha256 = "0h9n86kydxpa4qvkvf078zjqxazwxv9pvasf2rq7avdhr1asnppi";
+    }
+    else pkgs.fetchurl {
+      url = "https://github.com/purescript/psc-package/releases/download/v0.5.1/linux64.tar.gz";
+      sha256 = "0nffiqhfdfkg0i9hgz3vhnjrwsb4gg912rmsn2gdfgbh1m44k5df";
+    };
 
   buildInputs = [ pkgs.gmp ];
 
@@ -26,7 +31,15 @@ in pkgs.stdenv.mkDerivation rec {
 
     install -D -m555 -T psc-package $PSC_PACKAGE
     chmod u+w $PSC_PACKAGE
+  '' + pkgs.stdenv.lib.optionalString pkgs.stdenv.isDarwin ''
+    install_name_tool \
+      -change /usr/lib/libSystem.B.dylib ${pkgs.darwin.Libsystem}/lib/libSystem.B.dylib \
+      -change /usr/lib/libiconv.2.dylib ${pkgs.libiconv}/libiconv.2.dylib \
+      $PSC_PACKAGE
+  '' + pkgs.stdenv.lib.optionalString (!pkgs.stdenv.isDarwin) ''
     patchelf --interpreter ${dynamic-linker} --set-rpath ${libPath} $PSC_PACKAGE
+  '' +
+  ''
     chmod u-w $PSC_PACKAGE
 
     mkdir -p $out/etc/bash_completion.d/
