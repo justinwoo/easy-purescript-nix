5840355396977eb0bd0192afb74aff5186f33435
toastal <toastal@protonmail.com>
2021-03-23 16:48:09 +0700

    add purescript-psa and build node packages with the same node version

---
+++ psa/default.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {
+    inherit system;
+  }
+, system ? builtins.currentSystem
+, nodejs ? pkgs."nodejs-12_x"
+}:
+
+let
+  nodeEnv = import ./node-env.nix {
+    inherit (pkgs) stdenv lib python2 runCommand writeTextFile;
+    inherit pkgs nodejs;
+    libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
+  };
+
+  nodePackage = import ./node-packages.nix
+    {
+      inherit (pkgs) fetchurl nix-gitignore stdenv lib fetchgit;
+      inherit nodeEnv;
+    };
+
+  source = nodePackage.sources."purescript-psa-0.8.2".src;
+in
+nodeEnv.buildNodePackage (nodePackage.args // { src = source; })
