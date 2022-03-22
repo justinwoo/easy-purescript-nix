5e5f26c449de40d391a0cd8dd99a116fafc91482
James Brock <jamesbrock@gmail.com>
2022-03-22 22:49:35 +0900

    Versioned symbols for ncurses
    For issue #188

--- purs/0.14.6.nix
+++ purs/0.14.6.nix
@@ -17,7 +17,14 @@ let
         sha256 = "01mf850a9jhqba6a3hsbl9fjxp2khplwnlr15wzp637s5vf7rd79";
       };
 
+  # Temporary fix for https://github.com/justinwoo/easy-purescript-nix/issues/188
+  pkgs_ncurses = pkgs.extend (self: super: {
+      ncurses5 = super.ncurses5.overrideAttrs (attr: {
+        configureFlags = attr.configureFlags ++ ["--with-versioned-syms"];
+      });
+    });
 in
 import ./mkPursDerivation.nix {
-  inherit pkgs version src;
+  inherit version src;
+  pkgs = pkgs_ncurses;
 }
