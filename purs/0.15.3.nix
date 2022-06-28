9f529cc88455b8ac6f070b6fb2e195363a2cd7b6
toastal <toastal@posteo.net>
2022-06-28 15:13:18 +0700

    purescript: 0.15.2 → 0.15.3

---
+++ purs/0.15.3.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  version = "v0.15.3";
+
+  src =
+    if pkgs.stdenv.isDarwin then
+      pkgs.fetchurl
+        {
+          url = "https://github.com/purescript/purescript/releases/download/${version}/macos.tar.gz";
+          sha256 = "01ka3j1aakcabw6kmkfbk4c2cl2prihk6g8zvc995l83snj4n403";
+        }
+    else # assume Linux
+      pkgs.fetchurl {
+        url = "https://github.com/purescript/purescript/releases/download/${version}/linux64.tar.gz";
+        sha256 = "083pd5l0q2dj4ky9r4sgnfiinlch73qcvl3l26jf3nfs48y8hffv";
+      };
+
+in
+import ./mkPursDerivation.nix {
+  inherit pkgs version src;
+}
