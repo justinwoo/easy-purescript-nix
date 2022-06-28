0cd6f267ac790dbe5aaa68c68c074d233ff21e2c
toastal <toastal@posteo.net>
2022-06-28 15:15:35 +0700

    Throw on unsupported platform
    Rather than assuming a) Linux and b) x86_64

--- purs/0.15.3.nix
+++ purs/0.15.3.nix
@@ -4,17 +4,18 @@ let
   version = "v0.15.3";
 
   src =
-    if pkgs.stdenv.isDarwin then
-      pkgs.fetchurl
-        {
-          url = "https://github.com/purescript/purescript/releases/download/${version}/macos.tar.gz";
-          sha256 = "01ka3j1aakcabw6kmkfbk4c2cl2prihk6g8zvc995l83snj4n403";
-        }
-    else # assume Linux
-      pkgs.fetchurl {
+    if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then
+      (pkgs.fetchurl {
         url = "https://github.com/purescript/purescript/releases/download/${version}/linux64.tar.gz";
         sha256 = "083pd5l0q2dj4ky9r4sgnfiinlch73qcvl3l26jf3nfs48y8hffv";
-      };
+      })
+    else if pkgs.stdenv.hostPlatform.system == "x86_64-darwin" then
+      (pkgs.fetchurl {
+        url = "https://github.com/purescript/purescript/releases/download/${version}/macos.tar.gz";
+        sha256 = "01ka3j1aakcabw6kmkfbk4c2cl2prihk6g8zvc995l83snj4n403";
+      })
+    else
+      throw "Architecture not supported";
 
 in
 import ./mkPursDerivation.nix {
