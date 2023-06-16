461b61fd58fa8cf880e4a2e8929b8c55cf929e1f
James Brock <jamesbrock@gmail.com>
2023-06-16 16:28:02 +0900

    deluxe shell psa and print versions

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
                   psc-package
                   purp
                   spago
+                  psa
                   spago2nix
                   pscid
                   purescript-language-server
@@ -39,6 +40,25 @@
                 source <(node --completion-bash)
                 echo -e "  \033[1measy-purescript-nix deluxe development environment\033[0m"
                 echo -e "  \033[1mSee https://discourse.purescript.org/t/recommended-tooling-for-purescript-in-2022\033[0m"
+                printf "%-25s" "  purs $(purs --version)"
+                printf "%-25s" "  $(pulp --version | head -1)"
+                printf "%-25s" "  psc-package $(psc-package --version)"
+                echo ""
+                printf "%-25s" "  spago $(spago --version)"
+                printf "%-25s" "  psa $(psa --version)"
+                printf "%-25s" "  pscid $(pscid --version)"
+                echo ""
+                printf "%-25s" "  purs-tidy $(purs-tidy --version)"
+                printf "%-25s" "  purs-backend-es $(purs-backend-es --version 2>&1)"
+                printf "%-25s" "  $(purty version)"
+                echo ""
+                printf "%-25s" "  node $(node --version)"
+                printf "%-25s" "  esbuild $(esbuild --version)"
+                printf "%-25s" "  zephyr $(zephyr --version)"
+                echo ""
+                printf "%-25s" "  bower $(bower --version)"
+                printf "%-50s" "  purescript-language-server $(purescript-language-server --version)"
+                echo ""
                 '';
               };
            };
